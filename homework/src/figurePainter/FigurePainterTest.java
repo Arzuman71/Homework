03438cf54b74427c4e5a26002d324e1cb2e40b12
Name Surname <example@mail.com>
2020-04-21 02:41:31 +0400

    myHomework

---
+++ homework/src/figurePainter/FigurePainterTest.java
@@ -0,0 +1,19 @@
+package figurePainter;
+
+public class FigurePainterTest {
+
+    public static void main(String[] args) {
+        FigurePainter fp = new FigurePainter();
+        fp.figureOne();
+        System.out.println();
+        fp.figureTwo();
+        System.out.println();
+        fp.figureThree();
+        System.out.println();
+        fp.figureFour();
+        System.out.println();
+        fp.figureFive();
+
+    }
+
+}
